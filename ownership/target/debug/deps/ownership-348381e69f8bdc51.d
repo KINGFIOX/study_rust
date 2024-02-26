5076de58586b49a484aa202b667a727ca9d901df
Silence Wong <82296578+KINGFIOX@users.noreply.github.com>
2024-02-26 14:56:49 +0800

    Delete ownership/target directory

--- ownership/target/debug/deps/ownership-348381e69f8bdc51.d
+++
@@ -1,5 +0,0 @@
-/Users/wangfiox/DOCS_OFFLINE/OBSIDIAN/rust/ownership/target/debug/deps/ownership-348381e69f8bdc51.rmeta: src/main.rs
-
-/Users/wangfiox/DOCS_OFFLINE/OBSIDIAN/rust/ownership/target/debug/deps/ownership-348381e69f8bdc51.d: src/main.rs
-
-src/main.rs:
