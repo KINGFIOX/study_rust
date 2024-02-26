59ba6487990b55536585cb359a08e2a2e66b493d
Silence Wong <82296578+KINGFIOX@users.noreply.github.com>
2024-02-26 14:57:02 +0800

    Delete compound/target directory

--- compound/target/debug/deps/compound-2f504c1698ab6bb2.d
+++
@@ -1,5 +0,0 @@
-/Users/wangfiox/DOCS_OFFLINE/OBSIDIAN/rust/compound/target/debug/deps/compound-2f504c1698ab6bb2: src/main.rs
-
-/Users/wangfiox/DOCS_OFFLINE/OBSIDIAN/rust/compound/target/debug/deps/compound-2f504c1698ab6bb2.d: src/main.rs
-
-src/main.rs:
